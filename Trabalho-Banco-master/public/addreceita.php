1b9417e47e01ca0a85dd8b597a2bf9bffdd32d5b
Mario Henrique <101577344+mariogE100@users.noreply.github.com>
2023-05-23 19:57:31 -0300

    Update addreceita.php

--- Trabalho-Banco-master/public/addreceita.php
+++ Trabalho-Banco-master/public/addreceita.php
@@ -6,14 +6,24 @@ $quantidades = $_POST ['quantidades'];
 $ingredientes = $_POST ['ingredientes'];
 $modo_preparo = $_POST ['modo-preparo'];
 
-$sql = "INSERT INTO receitas (nome, quantidades, ingredientes, modo_preparo) VALUES (:nome, :quantidades, :ingredientes, :modo_preparo)";
+$sql = "INSERT INTO receitas (nome,modo_preparo) VALUES (:nome,:modo_preparo)";
 $statement = $conn->prepare($sql);
 $statement->bindParam(':nome', $nome_receita);
-$statement->bindParam(':quantidades', $quantidades);
-$statement->bindParam(':ingredientes', $ingredientes);
 $statement->bindParam(':modo_preparo', $modo_preparo);
 $statement->execute();
 
+$sql = "INSERT INTO ingredientes (nome) VALUES (:nome)";
+$statement = $conn->prepare($sql);
+$statement->bindParam(':nome', $ingredientes);
+$statement->execute();
+
+$sql = "INSERT INTO quantidades (quantidade, unidade_de_medida) VALUES (:quantidade, :unidade_de_medida)";
+$statement = $conn->prepare($sql);
+$statement->bindParam(':quantidade', $quantidades);
+$statement->bindParam(':unidade_de_medida', $quantidades);
+$statement->bindParam(':unidade_de_medida', $quantidades);
+$statement->execute();
+
+
 include_once('homeSistema.php');
 ?>
-
